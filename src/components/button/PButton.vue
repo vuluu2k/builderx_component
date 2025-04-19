3f9a68133fb8a56c5840788fab897f19ead0fff9
vuluu <vuluu040320@gmail.com>
2025-04-19 17:13:34 +0700

    upate

---
+++ src/components/button/PButton.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import AButton from "ant-design-vue/es/button";
+
+defineProps({
+  label: {
+    type: String,
+    default: "",
+  },
+  size: {
+    type: String,
+    default: "md",
+  },
+});
+</script>
+
+<template>
+  <AButton v-bind="$attrs" :class="['p-btn', size]">
+    <slot>{{ label }}</slot>
+  </AButton>
+</template>
+
+<style>
+@import url('./button.scss');
+</style>
--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,16 +1,16 @@
 import { fn } from '@storybook/test';
 import type { Meta, StoryObj } from '@storybook/vue3';
 
-import Button from './Button.vue';
+import PButton from '../components/button/PButton.vue';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 const meta = {
   title: 'Example/Button',
-  component: Button,
+  component: PButton,
   // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
   argTypes: {
-    size: { control: 'select', options: ['small', 'medium', 'large'] },
+    size: { control: 'select', options: ['sm', 'md', 'lg'] },
     backgroundColor: { control: 'color' },
   },
   args: {
@@ -18,7 +18,7 @@ const meta = {
     // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
     onClick: fn(),
   },
-} satisfies Meta<typeof Button>;
+} satisfies Meta<typeof PButton>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
